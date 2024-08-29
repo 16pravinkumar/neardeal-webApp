81d606024bb7241db503c3ee6dc4155b415e6d69
Bharat Sharma <bharatsharma98971@gmail.com>
2024-08-30 02:39:18 +0530

    Refactor CreatePackage component to add a new package section and update styling

--- client/src/Pages/CreatePackage.jsx
+++ client/src/Pages/CreatePackage.jsx
@@ -392,7 +392,57 @@ const CreatePackage = () => {
                                 </div>
                             </div>
                         </div>
+
+                        <div className="spa">
+                            <div style={{ width: '10%' }}>
+                                <img src={play} />
+                                <span style={{ fontWeight: 'bold' }}>Sauna</span>
+                                <img src={more} />
+                            </div>
+                            <button>
+                                <img src={plus} />
+                                <span>Add Package</span>
+                            </button>
+                        </div>
+
+                        <div className='item'>
+                            <div className='left' style={{ width: '84%' }}>
+                                <img src='https://avatars.githubusercontent.com/u/97161064?v=4' />
+                                <div>
+                                    <span>Largest spa in Hong Kong</span>
+                                    <div>
+                                        <div><img style={{ width: 'max-content' }} src={eye1} /><span style={{ color: 'grey' }}>1000 views</span></div>
+                                        <div style={{ marginLeft: '10px' }}><img style={{ width: 'max-content' }} src={likes} /><span style={{ color: 'grey' }}>1000 likes</span></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='right' style={{ width: '20%', justifyContent: 'space-between' }}>
+                                <div>
+                                    <div className="toggle-switch">
+                                        <input
+                                            type="checkbox"
+                                            id="toggle"
+                                            className="toggle-checkbox"
+                                            checked={isChecked}
+                                            onChange={handleToggle}
+                                        />
+                                        <label htmlFor="toggle" className="toggle-label"></label>
+                                        {/* <span>Publish</span> */}
+                                    </div>
+
+                                    <span>Publish</span>
+                                </div>
+
+                                <div>
+                                    <img width={25} src={share} />
+                                    <img width={25} src={eye} />
+                                    <img width={25} src={edit1} />
+                                </div>
+                            </div>
+                        </div>
                     </div>
+
+                    
 
                 </div>
                 <div className="offcanvas offcanvas-end" id="demo">
