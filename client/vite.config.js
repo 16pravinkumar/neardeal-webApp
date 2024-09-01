1e5ff904f6d815930e5ce47b8b303252882c222a
Bharat Sharma <bharatsharma98971@gmail.com>
2024-09-01 19:11:54 +0530

    Refactor Quill styles import and update dependencies

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-quill','react-quill/dist/quill.snow.css'] // Add react-quill to external if necessary
-    }
-  }
+  // build: {
+  //   rollupOptions: {
+  //     external: ['react-quill','react-quill/dist/quill.snow.css'] // Add react-quill to external if necessary
+  //   }
+  // }
 });
