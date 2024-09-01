72f5efec048b457223569aa2f225b48d8f1756ae
Bharat Sharma <bharatsharma98971@gmail.com>
2024-09-01 18:54:04 +0530

    Refactor package.json and add proxy for wellness.neardeal

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: ['react-quill'] // Add react-quill to external if necessary
+    }
+  }
+});
