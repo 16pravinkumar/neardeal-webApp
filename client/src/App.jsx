15c5e89f9add132ad21c4f4bbce4b2779b92b753
Bharat Sharma <bharatsharma98971@gmail.com>
2024-09-12 18:15:02 +0530

    Refactor routing in App component and remove unnecessary code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -38,8 +38,7 @@ function App() {
         <Route path='/campaign/redeemcode' element={<CampaignRedeemRecord></CampaignRedeemRecord>}></Route>
         <Route path='/store-settings' element={<StoreSettings></StoreSettings>}></Route> */} 
        
-        
-       <Route path="/" exact element={<ProtectedRoutes Component={Booking} isUserLoggedIn={isUserLoggedIn} />} /> 
+        <Route path="/" exact element={<ProtectedRoutes Component={Booking} isUserLoggedIn={isUserLoggedIn} />} /> 
         <Route path="/analytics" exact element={<ProtectedRoutes Component={Analytics} isUserLoggedIn={isUserLoggedIn} />} />
         <Route path="/create-package" exact element={<ProtectedRoutes Component={CreatePackage} isUserLoggedIn={isUserLoggedIn} />} />
         <Route path="/package" exact element={<ProtectedRoutes Component={Package} isUserLoggedIn={isUserLoggedIn} />} />
@@ -49,8 +48,6 @@ function App() {
         <Route path="/campaign/analytics" exact element={<ProtectedRoutes Component={CampainAnalytics} isUserLoggedIn={isUserLoggedIn} />} />
         <Route path="/campaign/redeemcode" exact element={<ProtectedRoutes Component={CampaignRedeemRecord} isUserLoggedIn={isUserLoggedIn} />} />
         <Route path="/store-settings" exact element={<ProtectedRoutes Component={StoreSettings} isUserLoggedIn={isUserLoggedIn} />} />
-        
-
       </Routes>
     </Router>
   )
--- client/src/Components/CreatePackage.jsx
+++ client/src/Components/CreatePackage.jsx
@@ -494,8 +494,7 @@ const CreatePackage = () => {
                     {
                         active === 'limits'
                         &&
-                        <div className="limits">
-                            <motion.div initial={{ opacity: 0 }}
+                        <motion.div initial={{ opacity: 0 }}
                                 animate={{ opacity: 1 }}
                                 exit={{ opacity: 0 }}
                                 transition={{ duration: 1 }} className="right">
@@ -600,7 +599,6 @@ const CreatePackage = () => {
 
                                 </div>
                             </motion.div>
-                        </div>
                     }
                     {/* Availability */}
 
