8efc0876105407f8bf09b045bb7a7dc6ac6e0f0d
Bharat Sharma <bharatsharma98971@gmail.com>
2024-09-18 18:06:07 +0530

    Refactor Booking component to add offcanvas for editing booking and filtering packages

--- client/src/Pages/Booking.jsx
+++ client/src/Pages/Booking.jsx
@@ -344,6 +344,116 @@ const Booking = () => {
                         </ul>
                     </nav>
                 </div>
+
+                <div className="offcanvas offcanvas-end" id="demo">
+                    <div className="offcanvas-header mb-0">
+                        <h3 className="offcanvas-title">Edit Booking</h3>
+                        <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas"></button>
+                    </div>
+                    <div className="offcanvas-body">
+                        <p className="offCanInvid">Invoice ID: <span id="invoiceNo">1234567890</span></p>
+                        <p className="offCanCustName">Customer<br /><span id="custName">Qwerty</span></p>
+                        <div className="card mt-4">
+                            <div className="card-header offCanCardHead">
+                                <div className="row">
+                                    <div className="col-lg-6">
+                                        <p>Order Status</p>
+                                    </div>
+                                    <div className="col-lg-6">
+                                        <button type="button" className="btn btn-outline-dark offCanCardHeadBtn float-end mt-1">Save Changes</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card-body pb-0">
+                                <div className="row">
+                                    <select style={{ padding: '10px', borderRadius: '5px' }}>
+                                        <option>Attend</option>
+                                        <option>Cancel</option>
+                                        <option>Absent</option>
+                                        <option>Started</option>
+                                    </select>
+                                </div>
+                                <div style={{ marginTop: '10px' }} className="row">
+                                    <select style={{ padding: '10px', borderRadius: '5px' }}>
+                                        <option>Paid</option>
+                                        <option>Pending</option>
+                                    </select>
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div className="card mt-4">
+                            <div className="card-header offCanCardHead">
+                                <div className="row">
+                                    <div className="col-lg-6">
+                                        <p>Order Information</p>
+                                    </div>
+                                    <div className="col-lg-6">
+                                        <button type="button" className="btn btn-outline-dark offCanCardHeadBtn float-end mt-1">Save Changes</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card-body pb-0">
+                                <div className="row">
+                                    <div className="col-lg-2">
+                                        <img style={{ borderRadius: '5px' }} src="https://avatars.githubusercontent.com/u/97161064?v=4" width="100%" alt="Package" />
+                                    </div>
+                                    <div className="col-lg-8">
+                                        <p className="offCanOfferType">Regular</p>
+                                        <p className="offCanPackageDetail">Japanese Sakura & Sake Spa Ritual for Couple /Japanese sake bath + Aromatherapy Massage + Suite + Cake&Tea + Sake + Gym</p>
+                                    </div>
+                                </div>
+                                <div style={{ display: 'flex', justifyContent: 'space-between', margin: '10px 0px' }}>
+                                    <input type="date" style={{ borderRadius: '7px', width: '48%', textAlign: 'center' }} id="datepicker" placeholder="Date" />
+                                    <input type="time" style={{ borderRadius: '7px', width: '48%', textAlign: 'center' }} id="timepicker" placeholder="Time" />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="card mt-4">
+                            <div className="card-header offCanCardHead">
+                                <div className="row">
+                                    <div className="col-lg-12">
+                                        <p>Customer Information</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card-body ">
+                                <div className="row">
+                                    <div className="col-lg-4">
+                                        <img style={{ borderRadius: '50%' }} src="https://avatars.githubusercontent.com/u/97161064?v=4" width="100%" alt="Customer" />
+                                    </div>
+                                    <div className="col-lg-8 my-auto">
+                                        <p id="custName">Qwerty</p>
+                                    </div>
+                                </div>
+                                <div className="input-group mt-3 w-50">
+                                    <input type="text" className="form-control" id="cName" defaultValue="9012345678" />
+                                    <button className="btn btn-outline-secondary border-0" type="submit"><i className="fa fa-clone"></i></button>
+                                </div>
+                                <div className="input-group mb-3 mt-3 w-auto">
+                                    <input type="text" className="form-control" id="cEmail" defaultValue="someone@something.com" />
+                                    <button className="btn btn-outline-secondary border-0" type="submit"><i className="fa fa-clone"></i></button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="offcanvas offcanvas-end" id="filter-package">
+                    <div className="offcanvas-header mb-0">
+                        <h3 className="offcanvas-title">Filter Packagee</h3>
+                        <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas"></button>
+                    </div>
+                    <div className="offcanvas-body">
+                        <button className="remove-btn">Remove all filters</button>
+                        <div className="nav">
+                            <button className="booking-nav-active">Featured</button>
+                            <button>Spa</button>
+                            <button>Massage</button>
+                            <button>Sonna</button>
+                        </div>
+                    </div>
+                </div>
             </motion.div>
         </div>
     );
